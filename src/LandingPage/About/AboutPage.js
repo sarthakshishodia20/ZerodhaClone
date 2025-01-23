a9a429a8c585e74318496429920435c53a8f10b7
sarthakshishodia20 <sarthakshishodia2004@gmail.com>
2025-01-23 18:15:29 +0530

    lastFrontend

---
+++ src/LandingPage/About/AboutPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+import Team from './Team';
+import Hero from './Hero';
+
+function AboutPage() {
+    return ( <>
+    <div>
+        <Hero/>
+        <Team/>
+    </div>
+    </> );
+}
+
+export default AboutPage;
---
+++ src/LandingPage/SignUp/SignupPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUp from './SignUp';
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+
+function SignupPage() {
+    return ( <>
+    <div>
+        <SignUp/>
+    </div>
+    </> );
+}
+
+export default SignupPage;
